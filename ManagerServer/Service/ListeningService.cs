564dd45d78af21d51081777415f09fb7f5098161
Quang Dat <thanhdat8791@gmail.com>
2023-06-05 07:49:48 +0700

    [QuangDat]: note background Sẻvice

--- ManagerServer/Service/ListeningService.cs
+++ ManagerServer/Service/ListeningService.cs
@@ -1,74 +1,74 @@
-﻿using System;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using ManagerServer.Common.Constant;
-using ManagerServer.Database;
-using ManagerServer.Model.RawData;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
-using MongoDB.Driver.Core.Configuration;
-using uPLibrary.Networking.M2Mqtt;
-using uPLibrary.Networking.M2Mqtt.Messages;
+﻿//using System;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using ManagerServer.Common.Constant;
+//using ManagerServer.Database;
+//using ManagerServer.Model.RawData;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Hosting;
+//using MongoDB.Driver.Core.Configuration;
+//using uPLibrary.Networking.M2Mqtt;
+//using uPLibrary.Networking.M2Mqtt.Messages;
 
-namespace MyProject.Services
-{
-    public class ListeningService : BackgroundService
-    {
-        private readonly MqttClient _client;
-        private readonly string _clientId;
-        private readonly DbContextOptionsBuilder<ManagerDbContext> builder =
-        new DbContextOptionsBuilder<ManagerDbContext>().UseSqlServer(Constant.ConnectionStringSqlServer);
+//namespace MyProject.Services
+//{
+//    public class ListeningService : BackgroundService
+//    {
+//        private readonly MqttClient _client;
+//        private readonly string _clientId;
+//        private readonly DbContextOptionsBuilder<ManagerDbContext> builder =
+//        new DbContextOptionsBuilder<ManagerDbContext>().UseSqlServer(Constant.ConnectionStringSqlServer);
 
-        public ListeningService()
-        {
-            _client = new MqttClient(Constant.BrokerURL);
-            _clientId = Guid.NewGuid().ToString();
-        }
+//        public ListeningService()
+//        {
+//            _client = new MqttClient(Constant.BrokerURL);
+//            _clientId = Guid.NewGuid().ToString();
+//        }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            _client.MqttMsgPublishReceived += async (s, e) =>
-            {
-                await SaveToDb(e);
-            };
-            while (!stoppingToken.IsCancellationRequested)
-            {
+//        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+//        {
+//            _client.MqttMsgPublishReceived += async (s, e) =>
+//            {
+//                await SaveToDb(e);
+//            };
+//            while (!stoppingToken.IsCancellationRequested)
+//            {
 
-                try
-                {
-                    if (!_client.IsConnected)
-                    {
-                        _client.Connect(_clientId);
-                        Console.WriteLine("MQTT connected");
-                        _client.Subscribe(new string[] { Constant.SystemUrl + "/#" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
-                    }
+//                try
+//                {
+//                    if (!_client.IsConnected)
+//                    {
+//                        _client.Connect(_clientId);
+//                        Console.WriteLine("MQTT connected");
+//                        _client.Subscribe(new string[] { Constant.SystemUrl + "/#" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+//                    }
 
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("fail listening main" + e.Message);
-                }
-                await Task.Delay(2000, stoppingToken);
-            }
-        }
+//                }
+//                catch (Exception e)
+//                {
+//                    Console.WriteLine("fail listening main" + e.Message);
+//                }
+//                await Task.Delay(2000, stoppingToken);
+//            }
+//        }
 
-        private async Task<bool> SaveToDb(MqttMsgPublishEventArgs e)
-        {
-            try
-            {
-                //using (var context = new ManagerDbContext(builder.Options))
-                //{
-                //    //save
-                //}
-                return await Task.FromResult(true);
+//        private async Task<bool> SaveToDb(MqttMsgPublishEventArgs e)
+//        {
+//            try
+//            {
+//                //using (var context = new ManagerDbContext(builder.Options))
+//                //{
+//                //    //save
+//                //}
+//                return await Task.FromResult(true);
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("save erro:" + ex.Message);
-                throw;
-            }
-        }
-    }
-}
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("save erro:" + ex.Message);
+//                throw;
+//            }
+//        }
+//    }
+//}
