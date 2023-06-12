5c9925c33f9085edab34bff91b71abb40a75cd18
HuyHieu <huyhieu.hust2855@gmail.com>
2023-06-12 23:02:20 +0700

    [HuyHieu] Add models

---
+++ ManagerServer/Model/Device/RemoveDeviceActionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ManagerServer.Model.Device
+{
+    public class RemoveDeviceActionModel
+    {
+        public int ZoneId { get; set; }
+        public int DeviceActionId { get; set; }
+    }
+}
